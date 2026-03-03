19f57a9cabcdcbe9636800e1824eb6cfa524a55a
Yajurva <yajurvakesbhat2808@gmail.com>
2026-03-03 21:59:26 +0530

    Update adRoutes.js
    changed the logic of routes

--- backend/routes/adRoutes.js
+++ backend/routes/adRoutes.js
@@ -1,4 +1,4 @@
-const express = require("express");
+cont express = require("express");
 const router = express.Router();
 const { generateAd, getAdById } = require("../controllers/adController");
 
